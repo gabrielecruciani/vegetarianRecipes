a3d2e7798bf5887b4e029832fa12b7468047793e
gabrielecruciani <138598344+gabrielecruciani@users.noreply.github.com>
2024-01-09 17:42:14 +0100

    Add files via upload

---
+++ src/routes/Favourite.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Favourite() {
+  return (
+    <div className='favourite'>
+        <h1>Favourite</h1>
+    </div>
+  )
+}
+
+export default Favourite;
---
+++ src/routes/QuickRecipes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function QuickRecipes() {
+  return (
+    <div className='quick-recipes'>
+        <h1>Quick Recipes</h1>
+    </div>
+  )
+}
+
+export default QuickRecipes;
